f1b51445630d705cb3eb1b043b76137bc30174ae
AnoniemeBlobvis <top.arend@gmail.com>
2024-01-09 22:03:23 +0100

    Changed route-routing-resolve.service.ts again

--- src/main/webapp/app/entities/route/route/route-routing-resolve.service.ts
+++ src/main/webapp/app/entities/route/route/route-routing-resolve.service.ts
@@ -7,15 +7,15 @@ import { mergeMap } from 'rxjs/operators';
 import { IRoute } from '../route.model';
 import { RouteService } from '../service/route.service';
 
-export const routeResolve = (route: ActivatedRouteSnapshot): Observable<null | IRoute> => {
-  const id = route.params['id'];
+export const routeResolve = (activatedRoute: ActivatedRouteSnapshot): Observable<null | IRoute> => {
+  const id = activatedRoute.params['id'];
   if (id) {
     return inject(RouteService)
       .find(id)
       .pipe(
-        mergeMap((route: HttpResponse<IRoute>) => {
-          if (route.body) {
-            return of(route.body);
+        mergeMap((routeResponse: HttpResponse<IRoute>) => {
+          if (routeResponse.body) {
+            return of(routeResponse.body);
           } else {
             inject(Router).navigate(['404']);
             return EMPTY;
@@ -27,3 +27,34 @@ export const routeResolve = (route: ActivatedRouteSnapshot): Observable<null | I
 };
 
 export default routeResolve;
+
+// OLD METHOD
+// import { inject } from '@angular/core';
+// import { HttpResponse } from '@angular/common/http';
+// import { ActivatedRouteSnapshot, Router } from '@angular/router';
+// import { of, EMPTY, Observable } from 'rxjs';
+// import { mergeMap } from 'rxjs/operators';
+//
+// import { IRoute } from '../route.model';
+// import { RouteService } from '../service/route.service';
+//
+// export const routeResolve = (route: ActivatedRouteSnapshot): Observable<null | IRoute> => {
+//   const id = route.params['id'];
+//   if (id) {
+//     return inject(RouteService)
+//       .find(id)
+//       .pipe(
+//         mergeMap((route: HttpResponse<IRoute>) => {
+//           if (route.body) {
+//             return of(route.body);
+//           } else {
+//             inject(Router).navigate(['404']);
+//             return EMPTY;
+//           }
+//         }),
+//       );
+//   }
+//   return of(null);
+// };
+//
+// export default routeResolve;
